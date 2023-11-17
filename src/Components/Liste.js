4e253feccdff58bae696db0b03a089318441c921
Prapaline <124569549+Prapaline@users.noreply.github.com>
2023-11-17 16:21:06 +0100

    Add files via upload

--- src/Components/Liste.js
+++ src/Components/Liste.js
@@ -1,48 +1,139 @@
 import React, { useEffect, useState } from 'react';
 import '../App.css';
 
-export default function Mes_articles() {
-  const [blog, setBlog] = useState([]);
+export default function Liste() {
+  const [blog, setblog] = useState([]);
   const [affichage, setAffichage] = useState(false);
-  const [donneesNote, setDonneesNote] = useState({
-    note: ""
-  });
+  const [jeux, setJeux] = useState();
+  const [montantTotal, setMontantTotal] = useState(0);
 
-  const recupLocation = async () => {
-    const id = localStorage.getItem("key");
+  const [date_emprunt, setDate_emprunt] = useState(localStorage.getItem("date_emprunt"));
+  const [date_retour, setDate_retour] = useState(localStorage.getItem("date_retour"));
+
+  const recup = async () => {
     try {
-      const response = await fetch(`http://localhost:3008/location/${id}`, { method: "GET" });
-      if (response.status === 200) {
-        const data = await response.json();
-        setBlog(data);
-        setAffichage(true);
-      } else {
-        console.log("rien");
+      const response = await fetch(`http://localhost:3008/jeux`, { method: "GET" });
+
+      if (!response.ok) {
+        throw new Error(`Erreur HTTP! Statut : ${response.status}`);
       }
+
+      const data = await response.json();
+      setblog(data);
+      setAffichage(true);
     } catch (error) {
       console.error(error);
+      setAffichage(false);
     }
   };
 
+  const rechercher = async () => {
+    try {
+      const response = await fetch(`http://localhost:3008/jeux/titre`, { method: "GET" });
+
+      if (!response.ok) {
+        throw new Error(`Erreur HTTP! Status : ${response.status}`);
+      }
+
+      const data = await response.json();
+      setblog(data);
+      setAffichage(true);
+    } catch (error) {
+      console.error(error);
+      setAffichage(false);
+    }
+  };
+
+  const recupRecherche = async () => {
+    if (recherche.titre.trim() !== "") {
+      try {
+        const response = await fetch(`http://localhost:3008/jeux/${recherche.titre}`, { method: "GET" });
+
+        if (!response.ok) {
+          throw new Error(`Erreur HTTP! Status : ${response.status}`);
+        }
+
+        const data = await response.json();
+        setblog(data);
+        setAffichage(true);
+      } catch (error) {
+        console.error(error);
+        setblog([]);
+        setAffichage(false);
+      }
+    } else {
+      setblog([]);
+      setAffichage(true);
+    }
+  };
+
+  const soustraction = () => {
+    const emprunt = Date.parse(date_emprunt);
+    const retour = Date.parse(date_retour);
+
+    const recupPrix = async () => {
+      try {
+        const response = await fetch(`http://localhost:3008/jeuxP/${jeux}`, { method: "GET" });
+
+        if (!response.ok) {
+          throw new Error(`Erreur HTTP! Statut : ${response.status}`);
+        }
+
+        const data = await response.json();
+        setblog(data);
+        setAffichage(true);
+        setJeux(data);
+      } catch (error) {
+        console.error(error);
+        setAffichage(false);
+      }
+    };
+
+    recupPrix();
+
+    const difference = Math.abs(retour - emprunt);
+    const nJours = difference / (1000 * 3600 * 24);
+
+    let resultat = 0;
+
+    if (nJours > 0) {
+      resultat = nJours * parseFloat(jeux[0].prix);
+      setMontantTotal(resultat);
+    } else {
+      console.log("La date de retour doit être ultérieure à la date d'emprunt");
+      alert ("La date de retour doit être ultérieure à la date d'emprunt")
+    }
+  };
+
+  const [donnees, setDonnees] = useState({
+    id_location: null,
+    id_jeux: null,
+    utilisateurs_id: localStorage.getItem("key"),
+    date_emprunt: null,
+    date_retour: null,
+    note: null,
+    commentaires: null
+  });
+
+  const [recherche, setRecherche] = useState({
+    titre: ""
+  });
+
   useEffect(() => {
-    recupLocation();
+    recup();
+    rechercher();
   }, []);
 
-  const note = async (id_location, noteActuelle) => {
-    const id = localStorage.getItem("key");
-
-    // Utilisez l'id_location passé en paramètre pour identifier la location correcte
+  const ajoutLocation = async () => {
     try {
-      const response = await fetch(`http://localhost:3008/locationNote/${id_location}`, {
-        method: "PUT",
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ id_jeux: id_location, note: donneesNote.note })
-      });
+      console.log(donnees);
+      localStorage.setItem("date_emprunt", date_emprunt);
+      localStorage.setItem("date_retour", date_retour);
+      const reponse = await fetch(`http://localhost:3008/location`, { method: "POST", headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(donnees) });
 
-      if (response.status === 200) {
-        console.log(donneesNote);
-        // Effectuez une nouvelle requête pour récupérer les données mises à jour
-        recupLocation();
+      if (reponse.status === 200) {
+        console.log(reponse.status);
+        soustraction();
       }
     } catch (error) {
       console.error(error);
@@ -50,23 +141,48 @@ export default function Mes_articles() {
   };
 
   return (
-    <div>Mes locations :
-      {affichage ?
-        blog.map(jeux => (
-          <div cle={jeux.id_location}>
+    <div>
+      <div>
+        <br />
+        <center>
+          <input className="recherche" type="search" placeholder='Recherche' onChange={(e) => setRecherche({ ...recherche, titre: e.target.value })}></input>
+          <button className="recherche" onClick={() => recupRecherche()}>Rechercher</button>
+        </center>
+        <br />
+      </div>
+      <center>
+        <h1>Liste de tous nos Jeux Vidéo</h1>
+      </center>
+      {blog.length > 0 ?
+        blog.map(articles => (
+          <div key={articles.id}>
             <fieldset className='case'>
-              <p> Date emprunt : {jeux.date_emprunt}</p>
-              <p> Date retour : {jeux.date_retour} </p>
-              <p> Jeu n° : {jeux.id_jeux}</p>
-              <p> Note du jeu :{jeux.note}</p>
-              <p> Commentaire : {jeux.commentaires}</p>
-              <br />
-              <input type="number" placeholder='note entre 1 et 5' onChange={(e) => setDonneesNote({ ...donneesNote, note: e.target.value })}></input>
-              <button onClick={() => note(jeux.id_location, jeux.note)}>Noter</button>
-              <br />
+              <p> Jeu n° {articles.id_jeux}</p>
+              <p><u>Titre</u>: <i>{articles.titre}</i></p>
+              <p> {articles.texte}</p>
+              <p> <li>{articles.prix} euros</li></p>
+
+              {localStorage.getItem("key") != null ?
+                <div>
+                  <p><u>Louer ce jeu</u>:</p>
+                  <input type="text" placeholder='date_emprunt' onChange={(e) => { setDonnees({ ...donnees, date_emprunt: e.target.value }); setDate_emprunt(e.target.value) }} ></input>
+                  <br />
+                  <input type="text" placeholder='date_retour' onChange={(e) => { setDonnees({ ...donnees, date_retour: e.target.value }); setDate_retour(e.target.value) }} ></input>
+                  <br />
+                  <input type="text" placeholder='numéro du jeu' onChange={(e) => { setDonnees({ ...donnees, id_jeux: e.target.value }); setJeux(e.target.value) }}></input>
+                  <br />
+                  <button onClick={() => ajoutLocation()}>Ajouter</button>
+                </div>
+                : null}
+              <p>Montant total de la location : {montantTotal} euros</p>
             </fieldset>
           </div>
-        )) : <p>Chargement ...</p>}
+        )) :
+        (
+          <p>{blog.length === 0 ? 'Aucun jeu ne correspond à votre recherche.' : 'Chargement...'}</p>
+        )}
+      <div>
+      </div>
     </div>
-  );
+  )
 }
--- src/Components/Mes_articles.js
+++ src/Components/Mes_articles.js
@@ -1,40 +1,50 @@
 import React, { useEffect, useState } from 'react';
-import '../App.css';
+import '../App.css'
 
 export default function Mes_articles() {
-  const [blog, setBlog] = useState([]);
-  const [affichage, setAffichage] = useState(false);
+  //définition de termes
+    const [blog, setblog] = useState([]); 
+    const [affichage, setAffichage] = useState(false);
+    
 
-  const [donneesNote, setDonneesNote] = useState({
-    note: "",
-    commentaire: ""
-  });
+    const [donneesNote, setDonneesNote] = useState({
+        note :"",
+        commenatire:""
+    });
 
-  const recupLocation = async () => {
-    const id = localStorage.getItem("key");
-    try {
-      const response = await fetch(`http://localhost:3008/location/${id}`, { method: "GET" });
-      if (response.status === 200) {
-        const data = await response.json();
-        setBlog(data);
-        setAffichage(true);
-      } else {
-        console.log("rien");
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    //console.log(localStorage)
 
-  useEffect(() => {
-    recupLocation();
-  }, []);
+    //permet de récupérer les données de locations de l'utilisateur connecté
+    const recupLocation = async ()=>{
+        const id=localStorage.getItem("key")
+        //Chargement BDD
+        await fetch(`http://localhost:3008/location/${id}`, 
+        {method: "GET"})
+        .then(reponse => {
+            if (reponse.status === 200){
+                reponse.json().then(data => {
+                    setblog(data)
+                    setAffichage(true)
+                    console.log(data);
+                })
+            }else{
+                console.log("rien");
+            }
+        })
+          .catch(error => console.error(error));
+      };
 
-  const note = async (id_location, noteActuelle, key) => {
-    const id = localStorage.getItem("key");
+      //appelle la const recupLocation
+      useEffect(() => {
+        recupLocation()
+    },[])
 
-    // Vérifier que la note est entre 1 et 5
-    if (donneesNote.note >= 1 && donneesNote.note <= 5) {
+
+    //permet de donner une note à un jeu loué
+    const note = async (id_location, noteActuelle, key)=>{
+      const id=localStorage.getItem("key")
+      // Vérifier que la note est entre 1 et 5
+      if (donneesNote.note >= 1 && donneesNote.note <= 5) {
       try {
         await fetch(`http://localhost:3008/locationNote/${id_location}`, {
           method: "PUT",
@@ -48,6 +58,7 @@ export default function Mes_articles() {
           ...prevState,
           note: ""
         }));
+        window.location.reload();
       } catch (error) {
         console.error(error);
       }
@@ -55,48 +66,56 @@ export default function Mes_articles() {
       console.log("La note doit être entre 1 et 5");
       alert ("La note doit être entre 1 et 5")
     }
-  };
-
-  const commenter = async (id_location, key) => {
-    try {
-      await fetch(`http://localhost:3008/locationComm/${id_location}`, {
-        method: "PUT",
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ commentaires: donneesNote.commentaire })
-      });
-
-      console.log("Key récupérée :", key);
-
-      setDonneesNote(prevState => ({
-        ...prevState,
-        commentaire: ""
-      }));
-    } catch (error) {
-      console.error(error);
     }
-  };
+   
+    //permet de commenter le jeu loué
+    const commenter = async (id_location, key) => {
+      try {
+        await fetch(`http://localhost:3008/locationComm/${id_location}`, {
+          method: "PUT",
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ commentaires: donneesNote.commentaire })
+        });
+  
+        console.log("Key récupérée :", key);
+  
+        setDonneesNote(prevState => ({
+          ...prevState,
+          commentaire: ""
+        }));
+        window.location.reload();
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
   return (
-    <div>Mes locations :
-      {affichage ?
-        blog.map(jeux => (
-          <div key={jeux.id_location}>
-            <fieldset className='case'>
-              <p> Location n° : {jeux.id_location}</p>
-              <p> Date emprunt : {jeux.date_emprunt}</p>
-              <p> Date retour : {jeux.date_retour} </p>
-              <p> Jeu n° : {jeux.id_jeux}</p>
-              <p> Note du jeu :{jeux.note}</p>
-              <p> Commentaire : {jeux.commentaires}</p>
-              <br />
-              <input type="number" placeholder='note entre 1 et 5' onChange={(e) => setDonneesNote({ ...donneesNote, note: e.target.value })}></input>
+    <div >Mes locations :
+      {/* affichage */}
+        {affichage ? 
+         blog.map(jeux => (
+           <div>
+            {console.log(jeux)}
+             <fieldset className='case'>
+              {/* <p> Location n° : {jeux.id_location}</p> */}
+
+              {/* récupération et affichage des variables */}
+               <p> Date emprunt : {jeux.date_emprunt}</p>
+               <p> Date retour : {jeux.date_retour} </p>
+               <p> Jeu n° : {jeux.id_jeux}</p>
+               <p> Note du jeu :{jeux.note}</p>
+               <p> Commentaire : {jeux.commentaires}</p>
+               <br/>
+
+               {/* permet d'entrer des infos */}
+               <input type="number" placeholder='note entre 1 et 5' onChange={(e) => setDonneesNote({ ...donneesNote, note: e.target.value })}></input>
               <button onClick={() => note(jeux.id_location, jeux.note, localStorage.getItem("key"))}>Noter</button>
               <input type="text" placeholder='Commentaire' onChange={(e) => setDonneesNote({ ...donneesNote, commentaire: e.target.value })}></input>
               <button onClick={() => commenter(jeux.id_location, localStorage.getItem("key"))}>Commenter</button>
-              <br />
-            </fieldset>
-          </div>
-        )) : <p>Chargement ...</p>}
+               
+             </fieldset>
+           </div>
+         )) : <p>Chargement ...</p>}
     </div>
-  );
+  )
 }
